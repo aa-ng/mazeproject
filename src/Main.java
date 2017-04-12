c93f5b1cb22f881722524e5a3163c7618180d097
Cynicalbird <alex.ng.personal@gmail.com>
2017-04-12 19:05:44 -0400

    Renamed Run to Main for IDE compatibility

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+/** Runs the program
+ * @author Alex Ng 
+ */
+public class Main
+{
+	/** Run method, runs all implementation of the code.
+	 * @param args
+	 */
+	public static void main(String []args)
+	{
+		new GUI();
+	}
+}
